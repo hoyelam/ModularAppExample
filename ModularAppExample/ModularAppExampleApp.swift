56c9d8dbe008768be2e840a18219bcb6bc1a377c
Hoye Lam <hoyelam@protonmail.com>
2022-09-04 10:53:23 +0200

    Initial Commit

---
+++ ModularAppExample/ModularAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularAppExampleApp.swift
+//  ModularAppExample
+//
+//  Created by Hoye Lam on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ModularAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
